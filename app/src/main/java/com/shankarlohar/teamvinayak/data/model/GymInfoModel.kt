760f310c052cbcdd19397b4119fa14a384c4600b
shankarlohar <shankarloharmail@gmail.com>
2023-11-03 14:40:39 +0530

    created a gym info model

--- app/src/main/java/com/shankarlohar/teamvinayak/data/model/GymInfoModel.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/data/model/GymInfoModel.kt
@@ -1,4 +1,18 @@
 package com.shankarlohar.teamvinayak.data.model
 
-class GymInfoModel {
-}
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class GymInfoModel(
+    val logo: String = "",
+    val name: String = "",
+    val email: String = "",
+    val address: String = "",
+    val genderPreference: String = "",
+    val lateFee: Int = 0,
+    val lockerRental: Int = 0,
+    val monthlyFee: Int = 0,
+    val membershipHoldingFree: Int = 0,
+    val openToday: Boolean = false,
+    val phone: List<String> = emptyList(),
+    val timing: List<String> = emptyList()
+)
