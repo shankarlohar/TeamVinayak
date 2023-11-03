1a267642e329b2d1e70842cb222c5889f8c5e748
shankarlohar <shankarloharmail@gmail.com>
2023-11-03 14:41:32 +0530

    created a repository for firestore

--- app/src/main/java/com/shankarlohar/teamvinayak/data/repositories/FirestoreRepository.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/data/repositories/FirestoreRepository.kt
@@ -1,4 +1,15 @@
 package com.shankarlohar.teamvinayak.data.repositories
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.shankarlohar.teamvinayak.data.model.GymInfoModel
+import kotlinx.coroutines.tasks.await
+
 class FirestoreRepository {
+    private val db = FirebaseFirestore.getInstance()
+    private val gymInfoDocument = db.collection("vmg").document("info")
+
+    suspend fun getGymInfoDocument(): GymInfoModel? {
+        val snapshot = gymInfoDocument.get().await()
+        return snapshot.toObject(GymInfoModel::class.java)
+    }
 }
