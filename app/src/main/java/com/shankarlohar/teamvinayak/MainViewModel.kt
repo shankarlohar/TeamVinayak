1abcd71bcc49beb5c2d95d219e1140e018058d37
shankarlohar <shankarloharmail@gmail.com>
2023-11-03 14:42:14 +0530

    fetched firestore repository data to mainviewmodel

--- app/src/main/java/com/shankarlohar/teamvinayak/MainViewModel.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/MainViewModel.kt
@@ -1,23 +1,34 @@
 package com.shankarlohar.teamvinayak
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import com.shankarlohar.teamvinayak.data.model.GymInfoModel
+import com.shankarlohar.teamvinayak.data.repositories.FirestoreRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel(
+    firestoreRepository: FirestoreRepository
+) : ViewModel() {
+
 
     private val _isLoading = MutableStateFlow(true)
+    private val _gymInfo = MutableStateFlow<GymInfoModel?>(null)
 
     val isLoading = _isLoading.asStateFlow()
-
+    val gymInfo = _gymInfo.asStateFlow()
 
     init {
         viewModelScope.launch {
-            // launch important processes
-            _isLoading.value = false
+            try {
+                _gymInfo.value = firestoreRepository.getGymInfoDocument()
+            } catch (e: Exception) {
+                Log.e("firestore", "Error fetching GymInfo data: ${e.message}")
+            } finally {
+                _isLoading.value = false // Data loading is complete
+            }
         }
     }
 
