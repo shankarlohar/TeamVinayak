9312c2fd2c76904cf0aa2759e914a317001ad28e
shankarlohar <shankarloharmail@gmail.com>
2023-11-03 14:42:55 +0530

    passed firestore repo to view model and ui components

--- app/src/main/java/com/shankarlohar/teamvinayak/MainActivity.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.shankarlohar.teamvinayak
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.google.accompanist.pager.ExperimentalPagerApi
+import com.shankarlohar.teamvinayak.data.repositories.FirestoreRepository
 import com.shankarlohar.teamvinayak.ui.authentication.AuthViewModel
 import com.shankarlohar.teamvinayak.ui.authentication.signup.OnBoardingViewModel
 import com.shankarlohar.teamvinayak.ui.theme.TeamVinayakTheme
@@ -20,11 +23,10 @@ enum class MenuState {
 
 class MainActivity : ComponentActivity() {
 
-    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var mainViewModel: MainViewModel
     private val authViewModel: AuthViewModel by viewModels()
     private val onBoardingViewModel: OnBoardingViewModel by viewModels()
 
-    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,17 +36,28 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+        val firestoreRepository = FirestoreRepository()
+        mainViewModel = MainViewModel(firestoreRepository)
+
+
+
         val onBoardingContent = onBoardingViewModel.onBoardingData.value
+
 
 
         setContent {
 
             TeamVinayakTheme {
-                GymApp(
-                    authViewModel = authViewModel,
-                    onBoardingContent = onBoardingContent,
-                    context = LocalContext.current
-                )
+                val isLoading by mainViewModel.isLoading.collectAsState()
+
+                if (!isLoading) {
+                    GymApp(
+                        authViewModel = authViewModel,
+                        mainViewModel = mainViewModel,
+                        onBoardingContent = onBoardingContent,
+                        context = LocalContext.current
+                    )
+                }
             }
         }
     }
