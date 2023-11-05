90dbcb79bbb07d92c5bac88b1125ef68bf285ef6
shankarlohar <shankarloharmail@gmail.com>
2023-11-05 14:57:39 +0530

    signup component a little modification in personal details. I'll do the rest later

--- app/src/main/java/com/shankarlohar/teamvinayak/ui/authentication/signup/SignupComponent.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/ui/authentication/signup/SignupComponent.kt
@@ -1,58 +1,321 @@
 package com.shankarlohar.teamvinayak.ui.authentication.signup
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.shankarlohar.teamvinayak.R
+import com.shankarlohar.teamvinayak.model.ConsentToExercise
+import com.shankarlohar.teamvinayak.model.DoctorDetails
+import com.shankarlohar.teamvinayak.model.EmergencyContact
+import com.shankarlohar.teamvinayak.model.GymUserModel
+import com.shankarlohar.teamvinayak.model.PersonalDetails
 
-@Preview(showBackground = true)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview()
 @Composable
 fun SignupPage() {
-    Box {
-        SignupBgCard()
-        SignupMainCard()
-    }
-}
+    var gymUserModel by remember { mutableStateOf(GymUserModel()) }
+    var currentStep by remember { mutableStateOf(0) }
+    val steps = listOf(
+        "Personal Details",
+        "Consent to Exercise",
+        "Emergency Contact",
+        "Doctor Details",
+        "Membership Plan",
+        "Declaration"
+    )
 
-@Composable
-fun SignupBgCard() {
-    val signupText = buildAnnotatedString {
-        append("Page ")
-        withStyle(SpanStyle(color = MaterialTheme.colorScheme.error)) {
-            append("1/10.")
-        }
-    }
-    Surface(color = MaterialTheme.colorScheme.errorContainer, modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = signupText,
-                color = MaterialTheme.colorScheme.onErrorContainer,
-                modifier = Modifier
-                    .padding(bottom = 10.dp)
-                    .clickable {},
+    Surface(
+        color = MaterialTheme.colorScheme.onPrimary,
+        modifier = Modifier
+            .fillMaxHeight(0.9f)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(60.dp)
+            .copy(
+                topStart = ZeroCornerSize,
+                topEnd = ZeroCornerSize
             )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(6.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Display current step
+            Text(
+                text = steps[currentStep],
+                style = TextStyle(
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+
+            // Render content based on the current step
+            when (currentStep) {
+                0 -> PersonalDetailsSection(gymUserModel) { personalDetails ->
+                    gymUserModel = gymUserModel.copy(personalDetails = personalDetails)
+                    currentStep++
+                }
+                1 -> ConsentToExerciseSection(gymUserModel) { consentToExercise ->
+                    gymUserModel = gymUserModel.copy(consentToExercise = consentToExercise)
+                    currentStep++
+                }
+                2 -> EmergencyContactSection(gymUserModel) { emergencyContact ->
+                    gymUserModel = gymUserModel.copy(emergencyContact = emergencyContact)
+                    currentStep++
+                }
+                3 -> DoctorDetailsSection(gymUserModel) { doctorDetails ->
+                    gymUserModel = gymUserModel.copy(doctorDetails = doctorDetails)
+                    currentStep++
+                }
+                4 -> MembershipPlanSection(gymUserModel) { membershipPlan ->
+                    gymUserModel = gymUserModel.copy(membershipPlan = membershipPlan)
+                    currentStep++
+                }
+                5 -> DeclarationSection(gymUserModel) { declaration ->
+                    gymUserModel = gymUserModel.copy(declaration = declaration)
+                    // Here you can submit the `gymUser` object to your Firebase database or perform other actions.
+                }
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonalDetailsSection(gymUserModel: GymUserModel, onNext: (PersonalDetails) -> Unit) {
+    var personalDetails by remember { mutableStateOf(PersonalDetails()) }
+    val modifier = Modifier
+        .padding(8.dp)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+        Spacer(modifier = modifier)
+
+        // Add fields and input validation here
+        OutlinedTextField(
+            value = personalDetails.fullName.orEmpty(),
+            onValueChange = { personalDetails = personalDetails.copy(fullName = it) },
+            label = { Text("Full Name") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Person,
+                    contentDescription = stringResource(R.string.user_id)
+                )
+            }
+        )
+        Spacer(modifier = modifier)
+
+        OutlinedTextField(
+            value = personalDetails.address.orEmpty(),
+            onValueChange = { personalDetails = personalDetails.copy(address = it) },
+            label = { Text("Address") }
+        )
+        // Add more fields...
+
+        Button(
+            onClick = { onNext(personalDetails) },
+            colors = ButtonDefaults
+                .buttonColors(
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+            contentPadding = PaddingValues(16.dp),
+            shape = CircleShape,
+            modifier = modifier
+                .size(90.dp)
+        ) {
+            Text("Next")
+        }
+    }
+}
 
 @Composable
-fun SignupMainCard() {
-    Text(text = "this is to be done", modifier = Modifier.fillMaxSize())
+fun ConsentToExerciseSection(gymUserModel: GymUserModel, onNext: (ConsentToExercise) -> Unit) {
+    var consentToExercise by remember { mutableStateOf(ConsentToExercise()) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add fields and input validation here
+        Checkbox(
+            checked = consentToExercise.unableToExerciseInPast ?: false,
+            onCheckedChange = { consentToExercise = consentToExercise.copy(unableToExerciseInPast = it) },
+        )
+        Text("Unable to exercise in the past")
+        Checkbox(
+            checked = consentToExercise.physicianAdvisedAgainstExercising ?: false,
+            onCheckedChange = { consentToExercise = consentToExercise.copy(physicianAdvisedAgainstExercising = it) },
+        )
+        Text("Physician advised against exercising")
+        // Add more checkboxes...
+
+        Button(onClick = { onNext(consentToExercise) }) {
+            Text("Next")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmergencyContactSection(gymUserModel: GymUserModel, onNext: (EmergencyContact) -> Unit) {
+    var emergencyContact by remember { mutableStateOf(EmergencyContact()) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add fields and input validation here
+        TextField(
+            value = emergencyContact.contactName.orEmpty(),
+            onValueChange = { emergencyContact = emergencyContact.copy(contactName = it) },
+            label = { Text("Contact Name") }
+        )
+        TextField(
+            value = emergencyContact.relationship.orEmpty(),
+            onValueChange = { emergencyContact = emergencyContact.copy(relationship = it) },
+            label = { Text("Relationship") }
+        )
+    }
+    // Add more fields...
+
+    Button(onClick = { onNext(emergencyContact) }) {
+        Text("Next")
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DoctorDetailsSection(gymUserModel: GymUserModel, onNext: (DoctorDetails) -> Unit) {
+    var doctorDetails by remember { mutableStateOf(DoctorDetails()) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add fields and input validation here
+        TextField(
+            value = doctorDetails.doctorName.orEmpty(),
+            onValueChange = { doctorDetails = doctorDetails.copy(doctorName = it) },
+            label = { Text("Doctor Name") }
+        )
+        TextField(
+            value = doctorDetails.doctorContactNumber.orEmpty(),
+            onValueChange = { doctorDetails = doctorDetails.copy(doctorContactNumber = it) },
+            label = { Text("Doctor Contact Number") }
+        )
+    }
+    // Add more fields...
+
+    Button(onClick = { onNext(doctorDetails) }) {
+        Text("Next")
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MembershipPlanSection(gymUserModel: GymUserModel, onNext: (String) -> Unit) {
+    var membershipPlan by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add a Dropdown or other input method for selecting the plan
+        TextField(
+            value = membershipPlan,
+            onValueChange = { membershipPlan = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (membershipPlan.isNotBlank()) {
+                        onNext(membershipPlan)
+                    }
+                }
+            ),
+            singleLine = true,
+            label = { Text("Select Membership Plan") }
+        )
+
+        Button(onClick = { onNext(membershipPlan) }) {
+            Text("Next")
+        }
+    }
+}
+
+@Composable
+fun DeclarationSection(gymUserModel: GymUserModel, onComplete: (Boolean) -> Unit) {
+    var agreedToTerms by remember { mutableStateOf(false) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add a Checkbox for agreeing to terms and conditions
+        Checkbox(
+            checked = agreedToTerms,
+            onCheckedChange = { agreedToTerms = it },
+        )
+        Text("I agree to the terms and conditions")
+
+        Button(onClick = {
+            onComplete(agreedToTerms)
+        }) {
+            Text("Submit")
+        }
+    }
 }
