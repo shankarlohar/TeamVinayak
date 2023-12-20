ac5243d94750cda455ebaffeb1aa3f927bab10d3
shankarlohar <shankarloharmail@gmail.com>
2023-12-20 19:12:38 +0530

    status check viewing UI card done

---
+++ app/src/main/java/com/shankarlohar/teamvinayak/ui/common/UserCard.kt
@@ -0,0 +1,150 @@
+package com.shankarlohar.teamvinayak.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter
+import com.shankarlohar.teamvinayak.R
+import com.shankarlohar.teamvinayak.util.Details
+import com.shankarlohar.teamvinayak.util.Status
+
+@Composable
+fun UserCard(
+    painter: AsyncImagePainter,
+    fullName: String?,
+    uid: String?,
+    membershipStatus: Status?,
+    registrationStatus: Details?,
+    formSubmissionTime: String?
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.primaryContainer,
+        modifier = Modifier
+            .height(220.dp)
+            .padding(10.dp),
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color(0xFF000000)
+                ) {
+                    if (membershipStatus != Status.PENDING){
+                        Text(
+                            text = if (membershipStatus == Status.BLOCKED) "Blocked Account" else "Active Account",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }else{
+                        Text(
+                            text = "New User Request",
+                            fontSize =  12.sp,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                if (fullName != null) {
+                    Text(
+                        text = fullName,
+                        fontSize =  24.sp,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(text = if (registrationStatus == Details.UNDER_REVIEW) "User details are under review." else "User details are verified.", fontSize = 8.sp)
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.user),
+                        tint = Color(0xFF000000),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    if (uid != null) {
+                        Text(
+                            text = uid,
+                            fontSize =  8.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        containerColor = Color.White
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    if (formSubmissionTime != null) {
+                        Text(
+                            text = formSubmissionTime.substring(0,11),
+                            fontSize =  11.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+
+            Surface(
+                shape = CircleShape,
+                modifier = Modifier.size(125.dp)
+            ) {
+                Image(
+                    painter = painter,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/shankarlohar/teamvinayak/ui/newuserside/component/newuser/sub/AccountStatus.kt
+++ app/src/main/java/com/shankarlohar/teamvinayak/ui/newuserside/component/newuser/sub/AccountStatus.kt
@@ -2,22 +2,17 @@ package com.shankarlohar.teamvinayak.ui.newuserside.component.newuser.sub
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -25,7 +20,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -34,8 +28,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -47,12 +39,10 @@ import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.shankarlohar.teamvinayak.R
-import com.shankarlohar.teamvinayak.model.UserData
-import com.shankarlohar.teamvinayak.util.Steps
+import com.shankarlohar.teamvinayak.ui.common.UserCard
 import com.shankarlohar.teamvinayak.viewmodel.AuthViewModel
 import com.shankarlohar.teamvinayak.viewmodel.UserViewModel
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -201,7 +191,9 @@ fun AccountStatus(
                 Column(
                     verticalArrangement = Arrangement.SpaceEvenly,
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
                 ) {
 
                     LaunchedEffect(scope) {
@@ -211,61 +203,14 @@ fun AccountStatus(
                             }
                         }
                     }
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Image(
-                            painter = painter,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(200.dp)
-                                .clip(MaterialTheme.shapes.medium)
-                                .padding(4.dp),
-                            contentScale = ContentScale.Fit
+                    UserCard(
+                        painter = painter,
+                        fullName = userData?.personalDetails?.fullName,
+                        uid = userData?.uid,
+                        membershipStatus = userData?.membership?.status,
+                        registrationStatus = userData?.membership?.details,
+                        formSubmissionTime = userData?.membership?.formSubmission
                         )
-                    }
-
-                    userData?.personalDetails?.let { personalDetails ->
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-
-                            ElevatedCard(modifier = Modifier.padding(2.dp)) { Text(personalDetails.fullName) }
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.padding(2.dp))
-
-                    userData?.let { userData ->
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            ElevatedCard(modifier = Modifier.padding(2.dp)) { Text(userData.uid) }
-                        }
-                    }
-
-                    userData?.membership?.let { membership ->
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Card(
-                            ) {
-                                Column(Modifier.padding(16.dp)) {
-                                    Text("Membership Status: " + membership.status.toString())
-                                    Text("Registration Form Status: " + membership.details)
-                                    Text("Form Submission: " + membership.formSubmission)
-                                }
-                            }
-                        }
-                    }
                 }
             }
         }
